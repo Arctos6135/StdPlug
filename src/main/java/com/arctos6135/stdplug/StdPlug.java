0ef501f7cef9973d652aa07ba3545cc59cfd85bb
Tyler Tian <tylertian123@gmail.com>
2019-08-15 16:04:36 -0400

    Import code from ImagesPlugin
    Import code from Arctos6135/ImagesPlugin and add it to StdPlug.

---
+++ src/main/java/com/arctos6135/stdplug/StdPlug.java
@@ -0,0 +1,22 @@
+package com.arctos6135.stdplug;
+
+import java.util.List;
+
+import com.arctos6135.stdplug.widgets.ImageWidget;
+
+import edu.wpi.first.shuffleboard.api.plugin.Description;
+import edu.wpi.first.shuffleboard.api.plugin.Plugin;
+import edu.wpi.first.shuffleboard.api.widget.ComponentType;
+import edu.wpi.first.shuffleboard.api.widget.WidgetType;
+
+@Description(group = "com.arctos6135", name = "StdPlug", version = "1.0.0", summary = "Arctos 6135 Standard Shuffleboard Plugin")
+public class StdPlug extends Plugin {
+
+    @Override
+    @SuppressWarnings("rawtypes")
+    public List<ComponentType> getComponents() {
+        return List.of(
+            WidgetType.forAnnotatedWidget(ImageWidget.class)
+        );
+    }
+}
---
+++ src/main/java/com/arctos6135/stdplug/widgets/ImageWidget.java
@@ -0,0 +1,122 @@
+package com.arctos6135.stdplug.widgets;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import edu.wpi.first.shuffleboard.api.prefs.Group;
+import edu.wpi.first.shuffleboard.api.prefs.Setting;
+import edu.wpi.first.shuffleboard.api.widget.AbstractWidget;
+import edu.wpi.first.shuffleboard.api.widget.AnnotatedWidget;
+import edu.wpi.first.shuffleboard.api.widget.Description;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+@Description(name = "Image", dataTypes = {}, summary = "Displays an image.")
+public class ImageWidget extends AbstractWidget implements AnnotatedWidget {
+
+    protected Pane imgPane;
+    protected ImageView view;
+
+    protected StringProperty filePath = new SimpleStringProperty();
+    protected BooleanProperty keepAspectRatio = new SimpleBooleanProperty(false);
+
+    protected double width = 0, height = 0;
+    // width / height
+    // Default is NaN, which indicates that no image was loaded
+    protected double aspectRatio = Double.NaN;
+
+    // Uses the window width and height to calculate and set the image width and height
+    protected void fitImage() {
+        if(!keepAspectRatio.get()) {
+            view.setFitWidth(width);
+            view.setFitHeight(height);
+        }
+        else {
+            // If the image was stretched to fit the height, how wide it would be
+            double scaledWidth = height * aspectRatio;
+
+            // The image would be too wide, so scale it so that it only fits the width
+            if(scaledWidth > width) {
+                double scaledHeight = width / aspectRatio;
+
+                view.setFitWidth(width);
+                view.setFitHeight(scaledHeight);
+            }
+            // The image would fit, so just use the calculated scaled width
+            else {
+                view.setFitWidth(scaledWidth);
+                view.setFitHeight(height);
+            }
+        }
+    }
+
+    public ImageWidget() {
+        // Create the ImageView that will hold the image
+        view = new ImageView();
+        // Create a BorderPane to put the ImageView in
+        imgPane = new BorderPane();
+        // Put the image in the center
+        ((BorderPane) imgPane).setCenter(view);
+        // Set the minimum size
+        // This is required, otherwise it won't resize properly when getting smaller
+        imgPane.setMinSize(0, 0);
+        
+        imgPane.widthProperty().addListener((observable, oldValue, newValue) -> {
+            width = newValue.doubleValue();
+            
+            if(!Double.isNaN(aspectRatio)) {
+                fitImage();
+            }
+        });
+        imgPane.heightProperty().addListener((observable, oldValue, newValue) -> {
+            height = newValue.doubleValue();
+
+            if(!Double.isNaN(aspectRatio)) {
+                fitImage();
+            }
+        });
+        
+        // When the file path is changed, load the new image
+        filePath.addListener((observable, oldValue, newValue) -> {
+            try {
+                Image img = new Image(new FileInputStream(newValue));
+                aspectRatio = img.getWidth() / img.getHeight();
+                view.setImage(img);
+                
+                // Re-calculate image size
+                fitImage();
+            }
+            catch(FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
+        // When keep aspect ratio is changed, re-calculate the image size
+        keepAspectRatio.addListener((observable, oldValue, newValue) -> {
+            if(!Double.isNaN(aspectRatio)) {
+                fitImage();
+            }
+        });
+    }
+
+    @Override
+    public List<Group> getSettings() {
+        return List.of(
+            Group.of("Image Options", 
+                Setting.of("Image File Path", filePath, String.class),
+                Setting.of("Keep Aspect Ratio", keepAspectRatio, Boolean.class)
+            )
+        );
+    }
+
+    @Override
+    public Pane getView() {
+        return imgPane;
+    }
+}
