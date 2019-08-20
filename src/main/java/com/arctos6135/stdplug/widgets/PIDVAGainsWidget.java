f87ae83e0c683d98105141b5c3c564b3acb3ff9e
Tyler Tian <tylertian123@gmail.com>
2019-08-20 02:20:55 -0400

    Add PIDVADP Gains support to PIDVAGainsWidget
    Add an additional kDP field to make PIDVAGainsWidget support both PIDVA
    and PIDVADP gains.

--- src/main/java/com/arctos6135/stdplug/widgets/PIDVAGainsWidget.java
+++ src/main/java/com/arctos6135/stdplug/widgets/PIDVAGainsWidget.java
@@ -1,6 +1,7 @@
 package com.arctos6135.stdplug.widgets;
 
 import com.arctos6135.stdplug.api.StdPlugWidgets;
+import com.arctos6135.stdplug.data.PIDVADPData;
 import com.arctos6135.stdplug.data.PIDVAData;
 
 import edu.wpi.first.shuffleboard.api.components.NumberField;
@@ -11,10 +12,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 
-@Description(name = StdPlugWidgets.PIDVA_GAINS, dataTypes = {
-        PIDVAData.class }, summary = "Displays a set of PIDVA Gains.")
+@Description(name = StdPlugWidgets.PIDVA_GAINS, dataTypes = { PIDVAData.class,
+        PIDVADPData.class }, summary = "Displays a set of PIDVA or PIDVADP Gains.")
 @ParametrizedController("PIDVAGainsWidget.fxml")
-public class PIDVAGainsWidget extends SimpleAnnotatedWidget<PIDVAData> {
+public class PIDVAGainsWidget extends SimpleAnnotatedWidget<Object> {
 
     @FXML
     private Pane root;
@@ -35,15 +36,27 @@ public class PIDVAGainsWidget extends SimpleAnnotatedWidget<PIDVAData> {
     private NumberField aField;
 
     @FXML
+    private NumberField dpField;
+
+    @FXML
     private void initialize() {
         // Add listener to the data property so that the displayed values are updated
         // when the data is updated
         dataProperty().addListener((observable, oldValue, newValue) -> {
-            pField.setNumber(newValue.kP);
-            iField.setNumber(newValue.kI);
-            dField.setNumber(newValue.kD);
-            vField.setNumber(newValue.kV);
-            aField.setNumber(newValue.kA);
+            if (newValue instanceof PIDVAData) {
+                pField.setNumber(((PIDVAData) newValue).kP);
+                iField.setNumber(((PIDVAData) newValue).kI);
+                dField.setNumber(((PIDVAData) newValue).kD);
+                vField.setNumber(((PIDVAData) newValue).kV);
+                aField.setNumber(((PIDVAData) newValue).kA);
+            } else {
+                pField.setNumber(((PIDVADPData) newValue).kP);
+                iField.setNumber(((PIDVADPData) newValue).kI);
+                dField.setNumber(((PIDVADPData) newValue).kD);
+                vField.setNumber(((PIDVADPData) newValue).kV);
+                aField.setNumber(((PIDVADPData) newValue).kA);
+                dpField.setNumber(((PIDVADPData) newValue).kDP);
+            }
         });
 
         // Add listener to the focused property of each field to trigger an action event
@@ -75,35 +88,95 @@ public class PIDVAGainsWidget extends SimpleAnnotatedWidget<PIDVAData> {
                 aField.getOnAction().handle(new ActionEvent(this, aField));
             }
         });
+        dpField.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                dpField.getOnAction().handle(new ActionEvent(this, dpField));
+            }
+        });
     }
 
     @FXML
     private void setP() {
-        PIDVAData data = new PIDVAData(pField.getNumber(), dataOrDefault.get().kI, dataOrDefault.get().kD, dataOrDefault.get().kV, dataOrDefault.get().kA);
+        Object data;
+        if (dataOrDefault.get() instanceof PIDVAData) {
+            data = new PIDVAData(pField.getNumber(), ((PIDVAData) dataOrDefault.get()).kI,
+                    ((PIDVAData) dataOrDefault.get()).kD, ((PIDVAData) dataOrDefault.get()).kV,
+                    ((PIDVAData) dataOrDefault.get()).kA);
+        } else {
+            data = new PIDVADPData(pField.getNumber(), ((PIDVADPData) dataOrDefault.get()).kI,
+                    ((PIDVADPData) dataOrDefault.get()).kD, ((PIDVADPData) dataOrDefault.get()).kV,
+                    ((PIDVADPData) dataOrDefault.get()).kA, ((PIDVADPData) dataOrDefault.get()).kDP);
+        }
         setData(data);
     }
 
     @FXML
     private void setI() {
-        PIDVAData data = new PIDVAData(dataOrDefault.get().kP, iField.getNumber(), dataOrDefault.get().kD, dataOrDefault.get().kV, dataOrDefault.get().kA);
+        Object data;
+        if (dataOrDefault.get() instanceof PIDVAData) {
+            data = new PIDVAData(((PIDVAData) dataOrDefault.get()).kP, iField.getNumber(),
+                    ((PIDVAData) dataOrDefault.get()).kD, ((PIDVAData) dataOrDefault.get()).kV,
+                    ((PIDVAData) dataOrDefault.get()).kA);
+        } else {
+            data = new PIDVADPData(((PIDVADPData) dataOrDefault.get()).kP, iField.getNumber(),
+                    ((PIDVADPData) dataOrDefault.get()).kD, ((PIDVADPData) dataOrDefault.get()).kV,
+                    ((PIDVADPData) dataOrDefault.get()).kA, ((PIDVADPData) dataOrDefault.get()).kDP);
+        }
         setData(data);
     }
 
     @FXML
     private void setD() {
-        PIDVAData data = new PIDVAData(dataOrDefault.get().kP, dataOrDefault.get().kI, dField.getNumber(), dataOrDefault.get().kV, dataOrDefault.get().kA);
+        Object data;
+        if (dataOrDefault.get() instanceof PIDVAData) {
+            data = new PIDVAData(((PIDVAData) dataOrDefault.get()).kP, ((PIDVAData) dataOrDefault.get()).kI,
+                    dField.getNumber(), ((PIDVAData) dataOrDefault.get()).kV, ((PIDVAData) dataOrDefault.get()).kA);
+        } else {
+            data = new PIDVADPData(((PIDVADPData) dataOrDefault.get()).kP, ((PIDVADPData) dataOrDefault.get()).kI,
+                    dField.getNumber(), ((PIDVADPData) dataOrDefault.get()).kV, ((PIDVADPData) dataOrDefault.get()).kA,
+                    ((PIDVADPData) dataOrDefault.get()).kDP);
+        }
         setData(data);
     }
 
     @FXML
     private void setV() {
-        PIDVAData data = new PIDVAData(dataOrDefault.get().kP, dataOrDefault.get().kI, dataOrDefault.get().kD, vField.getNumber(), dataOrDefault.get().kA);
+        Object data;
+        if (dataOrDefault.get() instanceof PIDVAData) {
+            data = new PIDVAData(((PIDVAData) dataOrDefault.get()).kP, ((PIDVAData) dataOrDefault.get()).kI,
+                    ((PIDVAData) dataOrDefault.get()).kD, vField.getNumber(), ((PIDVAData) dataOrDefault.get()).kA);
+        } else {
+            data = new PIDVADPData(((PIDVADPData) dataOrDefault.get()).kP, ((PIDVADPData) dataOrDefault.get()).kI,
+                    ((PIDVADPData) dataOrDefault.get()).kD, vField.getNumber(), ((PIDVADPData) dataOrDefault.get()).kA,
+                    ((PIDVADPData) dataOrDefault.get()).kDP);
+        }
         setData(data);
     }
 
     @FXML
     private void setA() {
-        PIDVAData data = new PIDVAData(dataOrDefault.get().kP, dataOrDefault.get().kI, dataOrDefault.get().kD, dataOrDefault.get().kV, aField.getNumber());
+        Object data;
+        if (dataOrDefault.get() instanceof PIDVAData) {
+            data = new PIDVAData(((PIDVAData) dataOrDefault.get()).kP, ((PIDVAData) dataOrDefault.get()).kI,
+                    ((PIDVAData) dataOrDefault.get()).kD, ((PIDVAData) dataOrDefault.get()).kV, aField.getNumber());
+        } else {
+            data = new PIDVADPData(((PIDVADPData) dataOrDefault.get()).kP, ((PIDVADPData) dataOrDefault.get()).kI,
+                    ((PIDVADPData) dataOrDefault.get()).kD, ((PIDVADPData) dataOrDefault.get()).kV,
+                    aField.getNumber(), ((PIDVADPData) dataOrDefault.get()).kDP);
+        }
+        setData(data);
+    }
+
+    @FXML
+    private void setDP() {
+        Object data;
+        if (dataOrDefault.get() instanceof PIDVAData) {
+            return;
+        } else {
+            data = new PIDVADPData(((PIDVADPData) dataOrDefault.get()).kP, ((PIDVADPData) dataOrDefault.get()).kI,
+                    ((PIDVADPData) dataOrDefault.get()).kD, ((PIDVADPData) dataOrDefault.get()).kV,
+                    ((PIDVADPData) dataOrDefault.get()).kA, dpField.getNumber());
+        }
         setData(data);
     }
 
