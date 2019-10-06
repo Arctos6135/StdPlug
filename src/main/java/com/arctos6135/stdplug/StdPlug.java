8ffe436d0d46e333f5a99fe6b25b042505e8cbc7
Tyler Tian <tylertian123@gmail.com>
2019-10-05 23:55:37 -0400

    Add documentation and update version
    Add documentation for the MJPEG Stream Viewer widget as Javadocs and in
    README and update version to 0.2.0.

--- src/main/java/com/arctos6135/stdplug/StdPlug.java
+++ src/main/java/com/arctos6135/stdplug/StdPlug.java
@@ -15,7 +15,7 @@ import edu.wpi.first.shuffleboard.api.plugin.Plugin;
 import edu.wpi.first.shuffleboard.api.widget.ComponentType;
 import edu.wpi.first.shuffleboard.api.widget.WidgetType;
 
-@Description(group = "com.arctos6135", name = "StdPlug", version = "0.1.0", summary = "Arctos 6135 Standard Shuffleboard Plugin")
+@Description(group = "com.arctos6135", name = "StdPlug", version = "0.2.0", summary = "Arctos 6135 Standard Shuffleboard Plugin")
 public class StdPlug extends Plugin {
 
     @Override
